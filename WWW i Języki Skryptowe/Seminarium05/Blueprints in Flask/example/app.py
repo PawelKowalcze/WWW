dcc861e12b061c2e484cef6e1ed758abd9a115fb
PawelKowalcze <kowalczepawel2004@gmail.com>
2025-04-03 12:21:54 +0200

    sem

--- WWW i Języki Skryptowe/Seminarium05/Blueprints in Flask/example/app.py
+++ WWW i Języki Skryptowe/Seminarium05/Blueprints in Flask/example/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from auth import auth
-
+from dashboard import dashboard
 
 app = Flask(__name__)
 app.register_blueprint(auth, url_prefix='/auth')
-
+app.register_blueprint(dashboard, url_prefix='/dashboard')
 
 if __name__ == '__main__':
     app.run(debug=True)
